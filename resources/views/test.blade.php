43a2708efdca90026ca611a8c154dbf0b789f37d
Andres Lira <Andres Lira>
2017-01-22 17:38:53 -0600

    npm, handlebars js

---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Holi mundo</title>
+    <script src="{{ asset('../../node_modules/handlebars/dist/handlebars.js') }}" charset="utf-8"></script>
+    <script src="{{ asset('js/main.js') }}" charset="utf-8"></script>
+  </head>
+  <body>
+    <h1>Vista de prueba</h1>
+  </body>
+</html>
