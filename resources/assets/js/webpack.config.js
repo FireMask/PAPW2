0cbb65657ae730b7557b086aee3d5d920a99c51c
David Reyes <david.reyes.1994@hotmail.com>
2017-01-28 17:34:29 -0600

    se agrego vue como dependencia y se actualizo webpack.config.js

--- resources/assets/js/webpack.config.js
+++ resources/assets/js/webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 module.exports = {
     entry: {
         main: __dirname + "/main.js"
@@ -7,10 +8,19 @@ module.exports = {
         filename: "[name].bundle.js",
         chunkFilename: "[id].bundle.js"
     },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jquery: 'jquery',
+            'window.jQuery': 'jquery',
+            jQuery: 'jquery'
+        })
+    ],
     resolve: {
         alias: {
             'jquery': __dirname + '/../../../node_modules/jquery/dist/jquery.js',
-            'handlebars': __dirname + '/../../../node_modules/handlebars/dist/handlebars.js'
+            'handlebars': __dirname + '/../../../node_modules/handlebars/dist/handlebars.js',
+            'vue': __dirname + '/../../../node_modules/vue/dist/vue.js'
         }
     },
     module: {
