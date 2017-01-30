d17342b661ca4ccbde3c9b66c385be719dbe7787
Andres <andres.lira20@gmail.com>
2017-01-30 12:21:41 -0600

    Se agregaron los paquetes AdminLTE, Font-Awesome, Ionicons, y se establecieron las rutas de elixir para su uso/ Se puso una plantilla base de AdminLTE

--- gulpfile.js
+++ gulpfile.js
@@ -17,10 +17,14 @@ require('laravel-elixir-vue-2');
    mix.styles([
      'resources/assets/css/test1.css',
      'resources/assets/css/test2.css'
-   ], 'public/css/style.css'
-    ).
+   ], 'public/css/style.css').
    webpack([
      'resources/assets/js/main.js'
-   ], 'public/js/main.js'
-  );
+   ], 'public/js/dist/main.js').
+   copy('node_modules/admin-lte/dist', 'public/js/dist/AdminLTE').
+   copy('node_modules/bootstrap/dist', 'public/js/dist/Bootstrap').
+   copy('node_modules/font-awesome/css', 'public/js/dist/Font-Awesome/css').
+   copy('node_modules/font-awesome/fonts', 'public/js/dist/Font-Awesome/fonts').
+   copy('node_modules/ionicons/dist/css', 'public/js/dist/Ionicons/css').
+   copy('node_modules/jquery/dist', 'public/js/dist/jQuery/dist');
  });
