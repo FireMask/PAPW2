46f09bdd899894f87476d26726ed44f76d268e45
Andres <andres.lira20@gmail.com>
2017-03-18 21:18:43 -0600

    :'v

--- gulpfile.js
+++ gulpfile.js
@@ -18,9 +18,6 @@ require('laravel-elixir-vue-2');
      'resources/assets/css/test1.css',
      'resources/assets/css/landing.css'
    ], 'public/css/style.css').
-   webpack([
-     'resources/assets/js/main.js'
-   ], 'public/js/dist/main.js').
    copy('node_modules/admin-lte/dist', 'public/js/dist/AdminLTE').
    copy('node_modules/bootstrap/dist', 'public/js/dist/Bootstrap').
    copy('node_modules/font-awesome/css', 'public/js/dist/Font-Awesome/css').
@@ -28,5 +25,8 @@ require('laravel-elixir-vue-2');
    copy('node_modules/ionicons/dist/css', 'public/js/dist/Ionicons/css').
    copy('node_modules/jquery/dist', 'public/js/dist/jQuery/dist').
    copy('node_modules/datatables.net-bs', 'public/js/dist/DataTables').
-    copy('node_modules/bootstrap-checkbox/js', 'public/js/dist/Bootstrap-Checkbox');
+   copy('node_modules/bootstrap-checkbox/dist', 'public/js/dist/Bootstrap-Checkbox').
+   webpack([
+       'resources/assets/js/main.js'
+   ], 'public/js/dist/main.js');
  });
